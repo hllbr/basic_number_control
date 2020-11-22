1e2fc3830783e62b15c57c0a0b9937d2091c4fd2
Halil İbrahim KOÇAK <halibrahim.kocak@gmail.com>
2020-11-22 21:09:14 +0300

    Add files via upload

---
+++ Build_x.java
@@ -0,0 +1,51 @@
+
+package build_x;
+
+import java.util.Scanner;
+
+
+public class Build_x {
+
+ 
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("bir rakam gişirinde bulunmanız gerekmektedir.");
+        int rakam = scn.nextInt();
+        
+        if(rakam ==1){
+            System.out.println("1 rakamını yazdınız");
+        }
+        else if(rakam ==2){
+        System.out.println("2 rakamını yazdınız");
+        }
+        else if(rakam ==3){
+        System.out.println("3 rakamını yazdınız");
+        }
+        else if(rakam ==4){
+        System.out.println("4 rakamını yazdınız");
+        }
+        else if(rakam ==5){
+        System.out.println("5 rakamını yazdınız");
+        }
+        else if(rakam ==6){
+        System.out.println("6 rakamını yazdınız");
+        }
+        else if(rakam ==7){
+        System.out.println("7 rakamını yazdınız");
+        }
+        else if(rakam ==8){
+        System.out.println("8 rakamını yazdınız");
+        }
+        else if(rakam ==9){
+        System.out.println("9 rakamını yazdınız");
+        }
+        else if(rakam ==0){
+        System.out.println("0 rakamını yazdınız");
+        }
+        else{
+        System.out.println("yanlış bir değer girişi gerçekleşti");
+        }
+ 
+    }
+    
+}
